c37729de953f7b37a4e7eff765cd0970701ee236
Jan Martin <jan.krems@gmail.com>
2025-11-02 16:09:41 -0800

    fix type error in markdown pipe test file

--- src/app/pipes/markdown-pipe.spec.ts
+++ src/app/pipes/markdown-pipe.spec.ts
@@ -1,8 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
 import { MarkdownPipe } from './markdown-pipe';
 
 describe('MarkdownPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MarkdownPipe();
+  let pipe: MarkdownPipe;
+  let sanitizer: DomSanitizer;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MarkdownPipe],
+    });
+    sanitizer = TestBed.inject(DomSanitizer);
+    pipe = new MarkdownPipe(sanitizer);
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform markdown to HTML', () => {
+    const markdown = '**bold** and *italic*';
+    const result = pipe.transform(markdown);
+    expect(result).toBeTruthy();
+  });
+
+  it('should handle null input', () => {
+    const result = pipe.transform(null);
+    expect(result).toBe('');
+  });
+
+  it('should handle undefined input', () => {
+    const result = pipe.transform(undefined);
+    expect(result).toBe('');
   });
 });
