d9b5cdadf6bcdbd30c55601469728c31f7d0e5f2
Jan Martin <jan.krems@gmail.com>
2025-11-20 05:07:02 -0800

    consistently use latest vite and vitest

--- apps/web-sv/vite.config.ts
+++ apps/web-sv/vite.config.ts
@@ -3,6 +3,7 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { playwright } from '@vitest/browser-playwright';
 
 export default defineConfig({
   plugins: [
@@ -21,22 +22,19 @@ export default defineConfig({
         extends: './vite.config.ts',
         test: {
           name: 'client',
-          environment: 'browser',
           browser: {
             enabled: true,
-            provider: 'playwright',
+            provider: playwright(),
             instances: [{ browser: 'chromium' }],
           },
           include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
           exclude: ['src/lib/server/**'],
-          setupFiles: ['./vitest-setup-client.ts'],
         },
       },
       {
         extends: './vite.config.ts',
         test: {
           name: 'server',
-          environment: 'node',
           include: ['src/**/*.{test,spec}.{js,ts}', 'tests/stress/**/*.{test,spec}.{js,ts}'],
           exclude: ['src/**/*.svelte.{test,spec}.{js,ts}'],
         },
