f3f25d86ad1e3d1c62bbd9878db7015e16f27a23
Jan Olaf Martin <jan.krems@gmail.com>
2025-11-20 05:13:12 -0800

    consistently use latest vite and vitest (#167)

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     projects: [
-      // TODO: Make the app tests not depend on a specific cwd.
-      // "apps/*",
+      "apps/*",
       "libs/*",
     ],
   },
